807a77a53fdfa4834f56afe15468a275e2b2dfdb
Michael Herman <hermanmu@gmail.com>
2014-08-23 11:09:06 -0700

    initial

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michaelherman/Documents/repos/github/django-ajax-model-form/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
